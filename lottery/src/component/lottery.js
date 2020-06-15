36e11481ba7659e8effc6abd10bb5cacebe4526f
deepcloudlabs <deepcloudlabs@gmail.com>
2020-06-15 14:15:39 +0300

    implement draw logic

--- lottery/src/component/lottery.js
+++ lottery/src/component/lottery.js
@@ -4,30 +4,54 @@ export default class Lottery extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            numbers : [],
-            column : 1
+            numbers: [],
+            column: 1
         }
     }
 
     render() {
         return (
-          <div className="container">
-              <div className="card">
-                  <div className="card-header">
-                      <h3 className="card-title">Lottery</h3>
-                  </div>
-                  <div className="card-body">
-                      <div className="form-group">
-                          <label htmlFor="column">Column:</label>
-                          <input id="column"
-                                 name="column"
-                                 className="form-control"
-                                 type="text"></input>
-                          <button className="btn btn-success">Draw</button>
-                      </div>
-                  </div>
-              </div>
-          </div>
+            <div className="container">
+                <div className="card">
+                    <div className="card-header">
+                        <h3 className="card-title">Lottery</h3>
+                    </div>
+                    <div className="card-body">
+                        <div className="form-group">
+                            <label htmlFor="column">Column:</label>
+                            <input id="column"
+                                   name="column"
+                                   className="form-control"
+                                   type="text"></input>
+                            <button className="btn btn-success" onClick={this.draw()}>Draw</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         );
+    }
+
+    draw = () => { // arrow function, lambda expression
+        let numbers = [];
+        for (let i = 0; i < this.state.column; ++i) {
+            numbers.push(this.createLotteryNumbers());
+        }
+        this.setState({
+            numbers: numbers
+        })
+    }
+
+    createLotteryNumbers = () => {
+        let numbers = [];
+        while (numbers.length < 6) {
+            let number = this.createLotteryNumber();
+            if (!numbers.includes(number))
+                numbers.push(number);
+        }
+        return undefined;
+    }
+
+    createLotteryNumber = (min = 1, max = 49) => {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
     }
 }
