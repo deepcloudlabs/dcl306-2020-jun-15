19f291633cd865f03f698621ff1b5a656f25cf91
deepcloudlabs <deepcloudlabs@gmail.com>
2020-06-17 11:44:23 +0300

    show EmployeeListConnector

--- hr-frontend/src/App.js
+++ hr-frontend/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
 import EmployeeConnector from "./component/employee-connector";
+import EmployeeListConnector from "./component/employee-list-connector";
 
 function App() {
     return (
         <div className="container">
             <EmployeeConnector></EmployeeConnector>
+            <p></p>
+            <EmployeeListConnector></EmployeeListConnector>
         </div>
     );
 }
